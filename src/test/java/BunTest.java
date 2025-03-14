1d4c2188fd92cbd171c1ad10848442cbbcf5f9f0
Kostya Sinichkin <s606kv@yandex.ru>
2025-03-14 19:31:51 +0700

    исправлена логика тестов в BunTest, удалены моки

--- src/test/java/BunTest.java
+++ src/test/java/BunTest.java
@@ -1,31 +1,15 @@
-import jdk.jfr.Description;
 import org.junit.Before;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.mockito.Mock;
-import org.mockito.Mockito;
-import org.mockito.junit.MockitoJUnitRunner;
 import praktikum.Bun;
-import praktikum.Database;
-
-import java.util.List;
 
 import static org.junit.Assert.assertEquals;
 
-@RunWith(MockitoJUnitRunner.class)
 public class BunTest {
-
-    Bun bun;
-
-    @Mock
-    Database mockedDatabase;
+    private Bun bun;
 
     @Before
     public void setUp () {
-        Mockito.when(mockedDatabase.availableBuns()).thenReturn(List.of(new Bun("TestBunName", 1000)));
-        List<Bun> buns = mockedDatabase.availableBuns();
-        Mockito.verify(mockedDatabase, Mockito.times(1)).availableBuns();
-        bun = buns.get(0);
+        bun = new Bun("TestBunName", 1000);
     }
 
     @Test
